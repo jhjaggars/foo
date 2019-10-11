dd92a43d3bb1ccaa95d480c2a23e7ea797bba2d9
Jesse Jaggars <jjaggars@redhat.com>
2019-10-10 19:40:39 -0500

    breaking it on purpose
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	for {
-		fmt.Println("HI! Heyyo...I'm still slow")
-		time.Sleep(10 * time.Minute)
-	}
+	panic("I'm busted on purpose!")
 }
