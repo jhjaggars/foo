b572895083d8d3a794c1e7552a10869b9f8ce347
Jesse Jaggars <jjaggars@redhat.com>
2019-10-11 13:24:13 -0500

    counter
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var counter = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "foo_requests",
+	Help: "counter of http requests",
+})
+
 func main() {
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		counter.Inc()
 		w.WriteHeader(http.StatusOK)
 	}
 	http.HandleFunc("/", handler)
